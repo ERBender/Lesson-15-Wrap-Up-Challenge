dfa7cfd46665052eb85f65b52061224c59d1f9d3
Erwin Bender <bender@erbender.com>
2021-09-27 10:20:24 -0500

    Add Book class

---
+++ Lesson 15 Wrap Up Challenge/Models/Book.swift
@@ -0,0 +1,24 @@
+//
+//  Book.swift
+//  Lesson 15 Wrap Up Challenge
+//
+//  Created by Erwin Bender on 9/27/21.
+//
+
+import Foundation
+
+class Book: Identifiable {
+    
+    var id:Int
+    var title:String
+    var author:String
+    var isFavorite:Bool
+    var currentPage:Int
+    var rating:Int
+    var content:[String]
+    
+    init() {
+        
+        //call the get data function
+    }
+}
