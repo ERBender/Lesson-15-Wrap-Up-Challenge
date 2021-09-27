707384fda17768138ead85e106c2707ab4348936
Erwin Bender <bender@erbender.com>
2021-09-27 10:08:28 -0500

    Initial Commit

---
+++ Lesson 15 Wrap Up Challenge/Lesson_15_Wrap_Up_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_15_Wrap_Up_ChallengeApp.swift
+//  Lesson 15 Wrap Up Challenge
+//
+//  Created by Erwin Bender on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_15_Wrap_Up_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
